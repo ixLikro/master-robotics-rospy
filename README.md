7db1af6c5134652b8fdb1d14835911724ceceec1
Ole Schleeßelmann <likro@outlook.de>
2019-10-15 18:08:04 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-src
+# Master: Robotics
+## Working with ROS (Robot Operating System)
+
+
+### How to run this Code:
+- [Install ROS Kinetic on Ubuntu 16.04.](http://wiki.ros.org/kinetic/Installation/Ubuntu)
+- Create a catkin workspace:
+```
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+```
+- Build workspace:
+```
+cd ~/catkin_ws
+catkin_make
+source devel/setup.bash
+```
+- Clone this Repo inside **~/catkin_ws/src**:
+```
+cd ~/catkin_ws/src
+git clone https://github.com/ixLikro/master-robotics-rospy.git
+```
+- Do you want to work with gazebo and turtlebot3?  Then install ros dependencies and clone some turtlebot3 repositorie:
+```
+sudo apt-get install ros-kinetic-joy ros-kinetic-teleop-twist-joy ros-kinetic-teleop-twist-keyboard ros-kinetic-laser-proc ros-kinetic-rgbd-launch ros-kinetic-depthimage-to-laserscan ros-kinetic-rosserial-arduino ros-kinetic-rosserial-python ros-kinetic-rosserial-server ros-kinetic-rosserial-client ros-kinetic-rosserial-msgs ros-kinetic-amcl ros-kinetic-map-server ros-kinetic-move-base ros-kinetic-urdf ros-kinetic-xacro ros-kinetic-compressed-image-transport ros-kinetic-rqt-image-view ros-kinetic-gmapping ros-kinetic-navigation ros-kinetic-interactive-markers
+cd ~/catkin_ws/src
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+```
+For more information see [here](https://github.com/saimouli/frontier_exploration_turtlebot#turtlebot3-packages-installation) or the [eManuel](http://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/#turtlebot3-simulation-using-gazebo)
+- Build again:
+```
+cd ~/catkin_ws
+catkin_make
+```
+
+After a successful build you can start nodes. For Example the hello world turtle Example:
+
+Run in individual Terminals:
+```
+roscore
+```
+```
+rosrun turtlesim turtlesim_node
+```
+```
+rosrun hello_world Turtle_draw_house.py
+```
+
+Or a gazebo simulation (individual Terminals):
+```
+roscore
+```
+```
+export TURTLEBOT3_MODEL=burger
+roslaunch turtlebot3_gazebo turtlebot3_empty_world.launch
+```
+```
+rosrun hello_world TurtleBot3_drive.py
+```
---
+++ hello_world/src/scripts/Turtle_draw_house.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+import rospy
+import math
+from std_msgs.msg import String
+from turtlesim.srv import TeleportAbsolute
+from std_srvs.srv import Empty
+from turtlesim.srv import SetPen
+from geometry_msgs.msg import Twist
+
+velPublisher = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size=100)
+
+def init():
+    rospy.init_node('tutle_house_drawer', anonymous=True)
+    reset()
+    turnPenOff()
+
+#reset the hole canvas
+def reset():
+    req = rospy.ServiceProxy('reset', Empty)
+    req.wait_for_service()
+    req()
+
+#port the given turtle to the given position and the given angle (0 - 360)
+def teleportTo(x, y, angle = 0, turtle = 1):
+    req = rospy.ServiceProxy('turtle'+str(turtle)+'/teleport_absolute', TeleportAbsolute)
+    req.wait_for_service()
+    req(float(x),float(y), float(angle)/(180.0/3.14))
+
+#turn the pen of the given turtle off or on (False)
+def turnPenOff(off = True, turtle = 1):
+    req = rospy.ServiceProxy('turtle'+str(turtle)+'/set_pen', SetPen)
+    req.wait_for_service()
+    if (off == True):
+        req(255,255,255,1,1)
+    else:
+        req(255,255,255,1,0)
+
+#move turtle 1 straight foreward with the given speed(coordinate per second) to the given distace
+def move(distance, speed = 3.5):
+    # setup message
+    velMessage = Twist()
+    velMessage.linear.x = abs(speed)
+
+    #reset other attribute, we want only move in a straight line
+    velMessage.linear.y = 0
+    velMessage.linear.z = 0
+    velMessage.angular.x = 0
+    velMessage.angular.y = 0
+    velMessage.angular.z = 0
+
+    #remember current time
+    t0 = rospy.Time.now().to_sec()
+    currentDistance = 0
+
+    #Loop to move the turtle
+    r = rospy.Rate(500)
+    while(currentDistance < distance):
+        # Publish message and get current time 
+        velPublisher.publish(velMessage)
+        t1 = rospy.Time.now().to_sec()
+        # Calculate the already moved distance (formula: distance = speed * time)
+        currentDistance = speed * (t1 - t0)
+        r.sleep()
+
+    #Force the turtle to stop
+    velMessage.linear.x = 0
+    velPublisher.publish(velMessage)
+
+#turn turtle 1 in degrees (counterclockwise). With the given speed(degree per second)
+def turn(angle, speed = 60.0, clockwise = False):
+
+    #convert angles to radians
+    angularSpeed = speed * (3.14 / 180)
+    RadianAngle = angle * (3.14 / 180)
+
+    #setup message
+    velMessage = Twist()
+    if(clockwise == False):
+        velMessage.angular.z = abs(angularSpeed)
+    else:
+        velMessage.angular.z = -abs(angularSpeed)
+
+    #reset other attribute, we want to turn the 
+    velMessage.linear.x = 0
+    velMessage.linear.y = 0
+    velMessage.linear.z = 0
+    velMessage.angular.x = 0
+    velMessage.angular.y = 0
+
+    #remember current time
+    t0 = rospy.Time.now().to_sec()
+    currentAngle = 0
+
+    #Loop to turn the turtle
+    r = rospy.Rate(500)
+    while(currentAngle < RadianAngle):
+        #Publish message and get current time 
+        velPublisher.publish(velMessage)
+        t1 = rospy.Time.now().to_sec()
+        #Calculate the already moved distance (formula: distance = speed * time)
+        currentAngle = angularSpeed * (t1 - t0)
+        r.sleep()
+
+    #Force the turtle to stop
+    velMessage.linear.x = 0
+    velPublisher.publish(velMessage)
+
+
+def draw():
+    #port to beginning
+    reset()
+    turnPenOff()
+    teleportTo(8,2, 90)
+    turnPenOff(False)
+
+    #draw the house
+    #right to top and top roof line
+    move(5)
+    turn(90)
+    move(5)
+
+    #roof, from left to right
+    turn(90 + 45, clockwise=True)
+    move(5.0 * math.cos(math.pi/4))
+    turn(90, clockwise=True)
+    move(5.0 * math.cos(math.pi/4))
+
+    #diagonal from top right to bottom left
+    turn(90, clockwise=True)
+    move(math.sqrt(math.pow(5,2) + math.pow(5,2)))
+
+    #connect to the roof an the other diagonal line
+    turn(90 + 45, clockwise=True)
+    move(5)
+    turn(90 + 45, clockwise=True)
+    move(math.sqrt(math.pow(5,2) + math.pow(5,2)))
+
+    #last lane at the bottom
+    turn(45 + 90, clockwise=True)
+    move(5)
+
+
+    #drive away
+    turnPenOff()
+    turn(45)
+    move(2.2)
+
+#draw the house instant 
+def drawByPort():
+    #port to beginning
+    reset()
+    turnPenOff()
+    teleportTo(8,1)
+    turnPenOff(False)
+
+    #draw house with the help of the teleport service
+    teleportTo(8,1)
+    teleportTo(8,6)
+    teleportTo(3,6)
+    teleportTo(5.5,10)
+    teleportTo(8,6)
+    teleportTo(3,1)
+    teleportTo(3,6)
+    teleportTo(8,1)
+    teleportTo(3,1, 180)
+
+
+if __name__ == "__main__":
+    try:
+        init()
+    except rospy.ROSInterruptException: pass
+
+    #drawByPort()
+    draw()
+
